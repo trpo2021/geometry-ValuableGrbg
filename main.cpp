9cdae531100ca3d24590eaeaf58252424009bc2f
Vadim Maltsev <ValuableGrbg@users.noreply.github.com>
2021-02-20 00:17:04 +0700

    Impliment function to find parametrs of circle

--- main.cpp
+++ main.cpp
@@ -8,6 +8,15 @@ struct circle {
     int radius;
 };
 
+void find_circle_param(circle circ)
+{
+    float perimetr = 2 * 3.14 * circ.radius;
+    float area = 3.14 * circ.radius * circ.radius;
+
+    cout « " Perimetr: " « perimetr « endl;
+    cout « " Area: " « area « endl;
+}
+
 int main()
 {
     return 0;
